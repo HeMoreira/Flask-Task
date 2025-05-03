86e810c3f014689457be2ec665637ceea47e0e98
Henrique <henriquefmoreira2@gmail.com>
2025-05-03 10:10:17 -0300

    feat: reformulação do programa

--- app.py
+++ app.py
@@ -1,60 +1,94 @@
 #para ativar venv: .\env\Scripts\activate.ps1
 #para rodar o programa: python app.py
+"""
+OBJETIVO
 
+Desenvolver um site em que cadastramos uma lista de compras, onde listamos
+produtos com seus preços e, ao final, obtemos o preço da compra total
+"""
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flasktask.db'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    #completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    nome = db.Column(db.String(100), nullable=False)
+    quantidade = db.Column(db.Integer, default=0)
+    preco = db.Column(db.Float, default=1.0)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<Item %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        nome_produto = request.form['nome']
+        quantia_produto = request.form['quantidade']
+        preco_produto = request.form['preco']
+        novo_produto = Todo(nome=nome_produto, quantidade=quantia_produto, preco=preco_produto)
         try:
-            db.session.add(new_task)
+            db.session.add(novo_produto)
             db.session.commit()
             return redirect('/')
         except:
             return "Deu errado meu mano"
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        itens = Todo.query.all()
+        total = 0
+        for item in itens:
+            total+=item.preco*item.quantidade
+        total = f"R$ {total:,.2f}"
+        return render_template('index.html', itens=itens, total=total)
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+@app.route('/diminuir_quantidade/<int:id>')
+def diminuir_quantidade(id):
+    produto = Todo.query.get_or_404(id)
     try:
-        db.session.delete(task_to_delete)
+        if produto.quantidade > 0:
+            produto.quantidade = produto.quantidade - 1
+            db.session.commit()
+        return redirect('/')
+    except:
+        return "ocorreu um erro ao tentar alterar a quantidade do produto"
+
+
+@app.route('/aumentar_quantidade/<int:id>')
+def aumentar_quantidade(id):
+    produto = Todo.query.get_or_404(id)
+    try:
+        produto.quantidade = produto.quantidade + 1
         db.session.commit()
         return redirect('/')
     except:
-        return "Deus não quer que você delete essa task"
+        return "ocorreu um erro ao tentar alterar a quantidade do produto"
     
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-    if request.method == 'POST':
-        task.content = request.form['content']
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "Algo deu errado na atualização da tarefa"
-    else:
-        return render_template('update.html', task=task)
+@app.route('/alterar_preco/<int:id>', methods=['POST'])
+def alterar_preco(id):
+    produto = Todo.query.get_or_404(id)
+    novo_preco = request.form['alt_preco']
+    try:
+        produto.preco = novo_preco
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "ocorreu um erro ao tentar alterar o preço do produto"
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    produto = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(produto)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "ocorreu um erro ao tentar remover o produto"
+
+
 
 if __name__ == "__main__":
+    #with app.app_context():
+    #    db.create_all()
     app.run(debug=True)
